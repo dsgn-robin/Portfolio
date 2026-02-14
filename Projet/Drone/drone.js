2f4e78bf3c3d7b20bcaf5ce5a2a454d2b05e6246
dsgn-robin <dgn.robin@gmail.com>
2026-02-14 23:23:02 +0100

    Correctifs

--- Projet/Drone/drone.js
+++ Projet/Drone/drone.js
@@ -11,27 +11,6 @@ if (boutonUp && card) {
 } else {
 }
 
-const BtnSTPS = document.querySelector(".ppcpc-r-c-btnst-s-1");
-const VideoPS = document.querySelector(
-  ".ppcpc-r-content-s-1 video:nth-child(1)",
-);
-const VideoSTPS = document.querySelector(
-  ".ppcpc-r-content-s-1 video:nth-child(2)",
-);
-const Icon1BtnSTPS = document.querySelector(".icon-s-subtitle-1");
-const Icon2BtnSTPS = document.querySelector(".icon-s-subtitle-2");
-
-BtnSTPS.onclick = function () {
-  VideoPS.classList.toggle("open");
-  VideoSTPS.classList.toggle("open");
-  Icon1BtnSTPS.classList.toggle("open");
-  Icon2BtnSTPS.classList.toggle("open");
-  VideoPS.muted = !VideoPS.muted;
-  VideoSTPS.muted = !VideoSTPS.muted;
-  VideoSTPS.currentTime = 0;
-  VideoPS.currentTime = 0;
-};
-
 document.addEventListener("DOMContentLoaded", () => {
   const revealElements = document.querySelectorAll(".reveal");
 
